3c2631834a008393da9692e6622142f1adf8b98d
Zakai Hamilton <zakaihamilton@gmail.com>
2021-04-04 18:03:26 +0300

    bug fix

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,29 @@
 import Head from 'next/head'
 import Util from "@components/Core/Util"
-import Main from "@components/Core/Layout/Main"
 import SplitPane from "@components/Core/Widgets/SplitPane"
 import Pane from "@components/Core/Widgets/SplitPane/Pane"
 
 export default function App() {
   return (
     <Util>
-      <Main>
-        <Head>
-          <title>System Concepts</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <SplitPane>
-          <Pane divider={true} style={{ backgroundColor: "red" }}>
+      <Head>
+        <title>System Concepts</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SplitPane>
+        <Pane divider={true} style={{ backgroundColor: "red" }}>
 
-          </Pane>
-          <Pane divider={true} style={{ backgroundColor: "orange" }}>
+        </Pane>
+        <Pane divider={true} style={{ backgroundColor: "orange" }}>
 
-          </Pane>
-          <Pane divider={true} style={{ backgroundColor: "green" }}>
+        </Pane>
+        <Pane divider={true} style={{ backgroundColor: "green" }}>
 
-          </Pane>
-          <Pane divider={true} style={{ backgroundColor: "blue" }}>
+        </Pane>
+        <Pane divider={true} style={{ backgroundColor: "blue" }}>
 
-          </Pane>
-        </SplitPane>
-      </Main>
+        </Pane>
+      </SplitPane>
     </Util>
   )
 }
--- src/Components/Core/Layout/Main.js
+++
@@ -1,7 +0,0 @@
-import styles from "./Main.module.scss"
-
-export default function Main({ children, ...props }) {
-    return <main className={styles.root} {...props}>
-        {children}
-    </main>;
-}
