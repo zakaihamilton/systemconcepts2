40bf8fae5282ccab652ed086b572c40eb9061e3e
Zakai Hamilton <zakaihamilton@gmail.com>
2021-04-07 22:39:35 +0300

    bug fix

--- src/Components/Core/UI/Layout/SplitPane/Pane.js
+++ src/Components/Core/UI/Layout/SplitPane/Pane.js
@@ -1,29 +1,32 @@
 import styles from "./Pane.module.scss"
 import { joinClasses } from "@util/styles"
 import SplitPane from "../SplitPane";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Pane({ classes, divider, children, size, style, ...props }) {
     const { orientation } = SplitPane.State.useState();
+    const [currentSize, setCurrentSize] = useState(size);
     const paneRef = useRef();
     const dividerRef = useRef();
     const list = SplitPane.List.useList(paneRef);
     const [dragging] = SplitPane.Drag.useDrag(dividerRef, paneRef, ({ orientation, percentage }) => {
         if (orientation === "vertical") {
-            paneRef.current.style.flex = `0 0 ${percentage.x}%`;
+            setCurrentSize(`0 0 ${percentage.x}%`);
         }
         else {
-            paneRef.current.style.flex = `0 0 ${percentage.y}%`;
+            setCurrentSize(`0 0 ${percentage.y}%`);
         }
     });
     const index = list.findIndex(ref => ref === paneRef.current);
     const isLast = index === list.length - 1;
     divider = divider && !isLast;
 
+    useEffect(() => {
+        setCurrentSize(`1 0 ${typeof size === "undefined" ? "auto" : size}`);
+    }, [size]);
+
     style = { ...style };
-    if (size) {
-        style.flex = `0 0 ${size ? size : "auto"}`;
-    }
+    style.flex = currentSize;
 
     return <div ref={paneRef} className={joinClasses(styles, { root: true, [orientation]: true }, classes?.root)} style={style} {...props}>
         <div className={joinClasses(styles, { pane: true, [orientation]: true }, classes?.pane)}>
