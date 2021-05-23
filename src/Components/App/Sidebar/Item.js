b7070a0bb69a75e8934c348bd8b5cef03254ca15
Zakai Hamilton <zakaihamilton@gmail.com>
2021-05-23 19:39:57 +0300

    bug fixes

--- src/Components/App/Sidebar/Item.js
+++ src/Components/App/Sidebar/Item.js
@@ -1,16 +1,20 @@
 import styles from "./Item.module.scss"
 import { FiChevronLeft, FiChevronDown } from "react-icons/fi";
 import Button from "@components/Core/UI/Widgets/Button";
+import Tooltip from "@components/Core/UI/Widgets/Tooltip";
 
-export default function SidebarItem({ name, children, depth, style }) {
+export default function SidebarItem({ name, children, depth, open, style }) {
     const hasChildren = children?.length;
-    const isOpen = false;
+    const isOpen = typeof open === "undefined" ? true : open;
     style = style || {};
     style.paddingLeft = depth * 16;
+    const titleTooltip = isOpen ? "Collapse" : "Expand";
     return <div className={styles.root} style={style}>
-        <Button style={{ visibility: hasChildren ? "visible" : "hidden" }}>
-            {!isOpen ? <FiChevronDown /> : <FiChevronLeft />}
-        </Button>
+        <Tooltip title={titleTooltip}>
+            <Button style={{ visibility: hasChildren ? "visible" : "hidden" }}>
+                {!!isOpen ? <FiChevronDown /> : <FiChevronLeft />}
+            </Button>
+        </Tooltip>
         <div className={styles.label}>
             {name}
         </div>
