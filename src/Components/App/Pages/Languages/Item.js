7edbbe7ea5e782b704654900cb5ce77c7b94d3af
Zakai Hamilton <zakaihamilton@gmail.com>
2021-06-17 23:28:56 +0300

    language switching support

--- src/Components/App/Pages/Languages/Item.js
+++ src/Components/App/Pages/Languages/Item.js
@@ -1,13 +1,20 @@
 import styles from "./Item.module.scss"
 import Translation from "@components/Core/Util/Translation"
+import Language from "@components/Core/Util/Language"
 import Bar from "@components/Core/UI/Layout/Bar"
+import clsx from "clsx"
 
 export default function LanguagesItem({ index, style }) {
     const languages = Translation.State.useState();
     const translation = Translation.useTranslation();
+    const languageState = Language.State.useState();
     const items = Object.keys(languages).map(key => languages[key]);
     const item = items[index];
-    return <Bar className={styles.root} style={style}>
+    const onClick = () => {
+        Object.assign(languageState, item);
+    };
+    const selected = languageState.id === item.id;
+    return <Bar onClick={onClick} className={clsx(styles.root, selected && styles.selected)} style={style}>
         <div className={styles.field}>{item?.name}</div>
         <div className={styles.field}>{item?.id}</div>
         <div className={styles.field}>{translation[item?.direction.toUpperCase()]}</div>
