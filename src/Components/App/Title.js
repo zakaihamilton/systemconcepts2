6dc0e7860fb80d94f49c1307ca100c27c3ff8eb7
Zakai Hamilton <zakaihamilton@gmail.com>
2021-06-25 15:59:05 +0300

    bug fix

--- src/Components/App/Title.js
+++ src/Components/App/Title.js
@@ -5,7 +5,7 @@ import AppButton from "./Title/AppButton";
 
 export default function Title() {
     return <Pane id="title" closable={false} size="3em" classes={styles}>
-        <Bar className={styles.bar}>
+        <Bar>
             <AppButton />
             <div style={{ flex: 1 }} />
         </Bar>
