e0a3295c9620b345c704fa96acc60dd39cf623cd
Zakai Hamilton <zakaihamilton@gmail.com>
2021-07-07 14:15:53 +0300

    bug fix

--- src/Components/App/Sidebar.js
+++ src/Components/App/Sidebar.js
@@ -4,22 +4,23 @@ import { createState } from "@components/Core/Util/State"
 import { useCallback, useEffect } from "react"
 import Tree, { treeMapper } from "@components/Core/UI/Layout/Tree"
 import SidebarItem from "./Sidebar/Item"
-import Size from "@components/Core/Util/Size"
 import items from "./Sidebar/Items"
 import { useLocation } from "@components/Core/Util/Location"
+import Layout from "src/Components/Core/Util/Layout"
 
 export default function Sidebar() {
+    const layout = Layout.useLayout();
     const location = useLocation();
     const sidebarState = Sidebar.State.useState();
     const visibleChanged = useCallback(visible => sidebarState.visible = visible, [sidebarState]);
-    const { width } = Size.useSize();
+    const isPopup = layout === "mobile";
     useEffect(() => {
         sidebarState.selected = location;
     }, [location, sidebarState]);
     const paneClasses = { root: styles.root, pane: styles.pane, divider: styles.divider };
     return <Pane.State visible={sidebarState?.visible}>
         <Pane.State.Notify visible={visibleChanged} />
-        <Pane classes={paneClasses} closable={false} divider={true} minSize={250} maxSize={500} size="20em">
+        <Pane classes={paneClasses} closable={false} divider={!isPopup} minSize={250} maxSize={500} size="20em">
             <Tree className={styles.list} root={items} itemSize={40} mapper={treeMapper} Item={SidebarItem} />
         </Pane>
     </Pane.State>;
