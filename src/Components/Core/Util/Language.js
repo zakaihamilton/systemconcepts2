2ea88d59ffdac0c11bb8543326ecc54825f15e8a
Zakai Hamilton <zakaihamilton@gmail.com>
2021-07-24 23:48:48 +0300

    bug fix

--- src/Components/Core/Util/Language.js
+++ src/Components/Core/Util/Language.js
@@ -1,5 +1,5 @@
 import { createState } from "@components/Core/Util/State";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import { createStorageHandler } from "@components/Core/Storage/Local";
 
 const storageHandler = createStorageHandler(["id", "direction", "name"]);
@@ -8,6 +8,9 @@ export default function Language({ children, direction, id, name }) {
     const updateDirection = useCallback(direction => {
         document.getElementsByTagName('html')[0].setAttribute("dir", direction);
     }, []);
+    useEffect(() => {
+        updateDirection(direction);
+    }, [direction, updateDirection]);
     return <Language.State direction={direction} id={id} name={name}>
         <Language.State.Notify direction={updateDirection} />
         <Language.State.Storage id="Language" {...storageHandler} />
