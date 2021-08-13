718d11235f447a66afd26f623d8853e8344f6ca7
Zakai Hamilton <zakaihamilton@gmail.com>
2021-08-13 09:22:18 +0300

    bug fix

--- src/Components/Core/UI/Layout/Table/Items.js
+++ src/Components/Core/UI/Layout/Table/Items.js
@@ -2,19 +2,33 @@ import { createState } from "@components/Core/Util/State";
 import TableLayout from "@components/Core/UI/Layout/Table"
 import { useMemo } from "react";
 
-export default function Items({ children, items }) {
+function useSort(items) {
     const tableState = TableLayout.State.useState();
     const { sortId, sortDirection } = tableState;
     items = useMemo(() => {
         const list = [...items];
         if (sortId) {
-            list?.sort((a, b) => b[sortId].toLowerCase() - a[sortId].toLowerCase());
+            list?.sort((a, b) => {
+                const aVal = a[sortId];
+                const bVal = b[sortId];
+                if (typeof aVal === "string" && typeof bVal === "string") {
+                    return aVal.toLowerCase().localeCompare(bVal.toLowerCase());
+                }
+                else {
+                    return aVal - bVal;
+                }
+            });
         }
         if (sortDirection === "asc") {
             list?.reverse();
         }
         return list;
     }, [items, sortDirection, sortId]);
+    return items;
+}
+
+export default function Items({ children, items }) {
+    items = useSort(items);
     return <Items.State items={items}>
         {children}
     </Items.State>;
